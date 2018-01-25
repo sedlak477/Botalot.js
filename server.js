a34f270ec01bef1d133f073c0768d1a3f0c56900
sedlak477 <michi.sedlak@gmail.com>
2018-01-25 19:20:32 +0100

    Add basic logging

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 const { Bot } = require("./bot/bot.js");
 const command = require("./bot/command.js");
+const log = require("loglevel");
 
+log.debug("Creating bot");
 let bot = new Bot();
 
+log.trace("Registering command managers");
 bot.registerCommandManager("!",
     new command.CommandManager(command.parseCommands(
         require("./bot/commandsets/commands.json")
     ))
 );
+log.debug("Command managers registered");
 
 bot.on("error", console.error);
-bot.on("login", () => console.log("Logged in"));
+bot.on("login", () => log.info("Logged in"));
 
 if (process.env.DISCORD_API_TOKEN) {
+    log.info("Logging in...");
     bot.login(process.env.DISCORD_API_TOKEN);
 } else {
-    console.error('Error: Environment variable "DISCORD_API_TOKEN" not found!');
+    log.error('Error: Environment variable "DISCORD_API_TOKEN" not found!');
     bot.close();
 }
 
